393e1e824d1d8eccd2fa232c50a5715fcc0757bb
Edsonbryan <nfek361@gmail.com>
2024-05-03 07:55:50 +0700

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by student on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
